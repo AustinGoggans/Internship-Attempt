dbacdd04639a1b797a9e8383237148e431534e60
Austin Goggans <Austin@austins-air.campus.tayloru.edu>
2017-05-08 22:02:02 -0400

    Almost done with first Story. Truncating Problems

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,12 @@
 class EmailsController < ApplicationController
   def index
-    @emails = Email.order(received_date: :desc).last(20)
+    if(params[:read]!=nil && params[:read]=='true')
+      @emails = Email.where(read_boolean:true).order(received_date: :desc).last(20)
+    elsif((params[:read]!=nil && params[:read]=='false'))
+      @emails = Email.where(read_boolean:false).order(received_date: :desc).last(20)
+    else
+      @emails = Email.order(received_date: :desc).last(20)
+    end
 
   end
 
--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -1,2 +1,9 @@
 module EmailsHelper
+  def shorten(string)
+    if(string.length > 75)
+      string[0..74] + "..."
+    else
+      string
+    end
+  end
 end
